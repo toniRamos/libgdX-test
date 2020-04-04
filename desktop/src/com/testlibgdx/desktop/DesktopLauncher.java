5f0c7e2e20373b2edf6843ddf38374bcd56d01cb
toniRamos <antonioramostorres@gmail.com>
2020-04-04 20:01:41 +0200

    Initial commit libgdx-test

---
+++ desktop/src/com/testlibgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.testlibgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.testlibgdx.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
